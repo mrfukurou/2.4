154e7a018a1a94012834d966a915b55b975c1232
fukur <fukur@LAPTOP-7EQMGM6H>
2020-05-07 20:58:42 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 4; i++, Console.WriteLine())
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(i + " ");
+                }
+                Console.WriteLine();
+                for (int m = 0; m < i; m++)
+                    Console.Write(5 + i + " ");
+            }
+        }
+    }
+}
